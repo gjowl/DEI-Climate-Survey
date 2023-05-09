c06ab000addc40a94be899259411fbc380f20fb6
gilbert.work <loiseau@wisc.edu>
2023-05-09 15:31:10 -0500

    remove any text data when reading file

--- code/climateSurveyAnalysis.py
+++ code/climateSurveyAnalysis.py
@@ -38,21 +38,21 @@ def countAnswers(df, q, answers):
     if '_' in q:
         # get any columns that match the question
         df_question = df.filter(regex=q)
-        # get the first row of the answer counts
+        # get the counted data for the question
         answer_counts = df_question.apply(pd.Series.value_counts).iloc[0]
-        # get the number of the column after the '_' for the answer_counts
+        # change the index to the numbered part of the input question
         answer_counts.index = answer_counts.index.str.split('_').str[1]
     else:
         # get the data for the question from the data file
         df_question = df[q]
         # get the column values
         answer_counts = df_question.value_counts()
-        # sort the answer count by the index
-        answer_counts = answer_counts.sort_index()
     return answer_counts
 
 # make a dataframe with the answers and counts for a given question, replacing index with the respective answer
 def getAnswerCountDf(answer_counts, answers):
+    # sort the answer counts by the index
+    answer_counts = answer_counts.sort_index()
     # convert the answer count index to a list
     unique_values = answer_counts.index.tolist()
     # get the smallest value in the unique values list; for some reason the survey center defined some answers with higher numbers than others, so this combats that
@@ -71,19 +71,19 @@ def getAnswerCountDf(answer_counts, answers):
     output_df = pd.DataFrame({'answer': unique_values, 'count': answer_counts.values})
     return output_df 
 
+# plot the bar graph for any percentage based questions
 def plotAverageBarGraph(df, question_number, output_dir):
     plt.title(f'{question_number}', fontsize = 10)
     plt.xlabel("Average Percent")
-    # plot the bar histogram for response vs count
     plt.barh(df['answer'], df['count'], color = 'teal')
     plt.savefig(f'{output_dir}/{question_number}.png', bbox_inches="tight")
     plt.clf()
 
+# plot the bar graph for any total count based questions
 def plotBarGraph(df, question_number, output_dir):
     s = int(df['count'].sum())
     plt.title(f'{question_number}, n={s}', fontsize = 10)
     plt.xlabel("Response count")
-    # plot the bar histogram for response vs count
     plt.barh(df['answer'], df['count'], color = 'teal')
     plt.savefig(f'{output_dir}/{question_number}.png', bbox_inches="tight")
     plt.clf()
@@ -110,6 +110,8 @@ if __name__ == '__main__':
 
     # read in the data file as a pandas dataframe with all columns as integers
     df_data = pd.read_csv(data_file, sep=',', header=0)
+    # remove any columns that contain 'TEXT'; this only analyzes the multiple choice questions
+    df_data = df_data.loc[:, ~df_data.columns.str.contains('TEXT')]
     # read in the answer file as a pandas dataframe
     df_answers = pd.read_csv(answer_file, sep=',', header=0)
 
@@ -127,15 +129,11 @@ if __name__ == '__main__':
             df_question = df_data.filter(regex=q)
             # hardcoding the list of answers for this question here
             q39_answers = ['Strongly disagree','Disagree','Neither agree nor disagree','Somewhat agree','Strongly agree','I do not know']
-            print(df_question)
             # loop through the columns and get the counts for each answer
             for col in df_question.columns:
                 answer_counts = df_question[col].value_counts()
-                # sort the answer counts by the index
-                answer_counts = answer_counts.sort_index()
                 # get the count of the answers
                 df_count = getAnswerCountDf(answer_counts, q39_answers)
-                print(df_count)
                 # get the number after the '_' in the column name
                 col_num = col.split('_')[1]
                 # get the question from the answer file by the column number
